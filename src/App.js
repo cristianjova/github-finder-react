f0a4ed25a6104079ea2ad8464501208eab9ac23e
Cristian Jovanovich <cristian.jova149@gmail.com>
2019-07-21 18:29:43 -0300

    created UserItem component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, {Component} from 'react';
-import Navbar from './components/layout/Navbar'
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
 import './App.css';
 
-
-class App extends Component{
+class App extends Component {
   render() {
     return (
-      <div className="app">
-          <Navbar title="Github Finder" icon="fab fa-github"/>
+      <div className='app'>
+        <Navbar title='Github Finder' icon='fab fa-github' />
+        <UserItem />
       </div>
     );
   }
