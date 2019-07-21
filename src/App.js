ce6f3a2b7e29f04c002b186dc3b6e7acfe36938c
Cristian Jovanovich <cristian.jova149@gmail.com>
2019-07-21 17:59:41 -0300

    created nabvar component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
+import Navbar from './components/layout/Navbar'
 import './App.css';
 
 
 class App extends Component{
-
   render() {
     return (
-      <div className="App">
-        <h1>Hello from React</h1>
+      <div className="app">
+          <Navbar title="Github Finder" icon="fab fa-github"/>
       </div>
     );
   }
