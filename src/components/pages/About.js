7009a61cbf407de5dd07376aa55e872f79947c49
Cristian Jovanovich <cristian.jova149@gmail.com>
2019-07-24 20:54:43 -0300

    added routes to pages and about component

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>Acerca de esta App</h1>
+      <p>App para buscar usuarios de Github</p>
+      <p>Versión: 1.0.0</p>
+    </Fragment>
+  );
+};
+
+export default About;
