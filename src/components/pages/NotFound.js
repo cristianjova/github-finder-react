36b055cfecc6dc7b29c58bb5e2401830a89231eb
Cristian Jovanovich <cristian.jova149@gmail.com>
2019-08-02 21:42:26 -0300

    added home and notfound component

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className='center'>
+      <h1>Página no encontrada</h1>
+      <p className='lead'>La página que estas buscando no existe</p>
+    </div>
+  );
+};
+
+export default NotFound;
